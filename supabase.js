3787f4b67b98faef55c56953eed40f1cb4bd031a
Sriraam N <90137595+Sriraam007@users.noreply.github.com>
2026-06-17 12:03:00 +0530

    Delete supabase.js

--- supabase.js
+++
@@ -1,4 +0,0 @@
-const SUPABASE_URL = 'https://hxpylxorvudmfugvtjvz.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh4cHlseG9ydnVkbWZ1Z3Z0anZ6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE0MjE3NjEsImV4cCI6MjA5Njk5Nzc2MX0.HLtVvpVuHzXa2UdEy723sl5_ZRgPbOiwF1Ovh7tMg7g';
-
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
